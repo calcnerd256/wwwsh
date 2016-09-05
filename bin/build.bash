34de2a497b7fcfb5b903b6ae3ebfea2047090189
Montana Rowe <calcnerd256@gmail.com>
2016-09-05 07:50:41 -0500

    moved addrinfo into a helper that opens the socket

--- bin/build.bash
+++ bin/build.bash
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+
 mkdir -p ../build/
 gcc \
  -g3 \
  -O3 \
  -Wall \
+ -Wpedantic \
+ -Wextra \
  -o ../build/main \
  ../src/main.c
--- src/main.c
+++ src/main.c
@@ -6,34 +6,33 @@
 #include <string.h>
 #include <stdio.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 
-struct addrinfo *server_address;
-int cleanup(int status){
-	if(server_address){
-		freeaddrinfo(server_address);
-		server_address = 0;
-	}
-	return status;
-}
 
-int main(int argument_count, char* *arguments_vector){
+int get_socket(char* port_number, int *out_sockfd){
+	struct addrinfo *server_address;
 	struct addrinfo hint;
 	char ipv4[INET_ADDRSTRLEN];
 	server_address = 0;
-	if(2 != argument_count) return cleanup(1);
 	memset(&hint, 0, sizeof hint);
 	hint.ai_family = AF_INET;
 	hint.ai_socktype = SOCK_STREAM;
 	hint.ai_flags = AI_PASSIVE;
-	if(getaddrinfo(0, arguments_vector[1], &hint, &server_address)) return cleanup(2);
+	if(getaddrinfo(0, port_number, &hint, &server_address)){
+		if(server_address){
+			freeaddrinfo(server_address);
+			server_address = 0;
+		}
+		return 2;
+	}
 	printf("%x %s %s %s %d %p <%s> %p\n", server_address->ai_flags,
 		server_address->ai_family == AF_INET ? "AF_INET" : "other?",
 		server_address->ai_socktype == SOCK_STREAM ? "SOCK_STREAM" : "other?",
 		getprotobynumber(server_address->ai_protocol)->p_name,
 		server_address->ai_addrlen,
-		server_address->ai_addr,
+		(void*)(server_address->ai_addr),
 		server_address->ai_canonname,
-		server_address->ai_next
+		(void*)(server_address->ai_next)
 	);
   if(AF_INET == server_address->ai_family){
 		inet_ntop(AF_INET, &(((struct sockaddr_in*)(server_address->ai_addr))->sin_addr), ipv4, INET_ADDRSTRLEN);
@@ -42,5 +41,30 @@ int main(int argument_count, char* *arguments_vector){
 			ntohs(((struct sockaddr_in*)(server_address->ai_addr))->sin_port)
 		);
 	}
-	return cleanup(0);
+  *out_sockfd = socket(server_address->ai_family, server_address->ai_socktype, server_address->ai_protocol);
+  if(-1 == *out_sockfd){
+		freeaddrinfo(server_address);
+		server_address = 0;
+		return 3;
+	}
+	if(bind(*out_sockfd, server_address->ai_addr, server_address->ai_addrlen)){
+		freeaddrinfo(server_address);
+		server_address = 0;
+		shutdown(*out_sockfd, SHUT_RDWR);
+		close(*out_sockfd);
+		return 4;
+	}
+	freeaddrinfo(server_address);
+	server_address = 0;
+	return 0;
+}
+
+int main(int argument_count, char* *arguments_vector){
+  int sockfd = -1;
+	if(2 != argument_count) return 1;
+  if(get_socket(arguments_vector[1], &sockfd)) return 2;
+	if(listen(sockfd, 32)) return 5;
+	if(shutdown(sockfd, SHUT_RDWR)) return 3;
+	if(close(sockfd)) return 4;
+  return 0;
 }

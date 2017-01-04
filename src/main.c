d12a7b14c3981c4559069f7d8d93ee24f996850c
Montana Rowe <calcnerd256@gmail.com>
2017-01-04 14:17:25 -0600

    don't confusingly reuse a variable that isn't named for reuse

--- src/main.c
+++ src/main.c
@@ -312,24 +312,24 @@ int contiguousHtmlResource_init(struct contiguousHtmlResource *res, char *url, c
 int main(int argument_count, char* *arguments_vector){
 	struct httpServer server;
 	struct mempool serverAllocations;
-	int ready_fd;
 	struct linked_list *match_node;
+	int ready_fd = -1;
+	int status = 0;
 
 	struct contiguousHtmlResource rootResourceStorage;
 
 	if(2 != argument_count) return 1;
 	if(httpServer_init(&server, &serverAllocations)) return 2;
 
-	ready_fd = contiguousHtmlResource_init(
+	status = contiguousHtmlResource_init(
 		&rootResourceStorage,
 		"/",
 		"<html>\r\n <head>\r\n  <title>Hello World!</title>\r\n </head>\r\n <body>\r\n  <h1>Hello, World!</h1>\r\n  <p>\r\n   This webserver is written in C.\r\n   I'm pretty proud of it!\r\n  </p>\r\n  <p>Coming soon: a resource for the following form to POST to</p>\r\n  <form method=\"POST\" action=\"formtest/\">\r\n   <input type=\"submit\" />\r\n  </form>\r\n </body>\r\n</html>\r\n\r\n"
 	);
-	if(ready_fd) return 3;
-	ready_fd = httpServer_pushResource(&server, &(rootResourceStorage.link_node), &(rootResourceStorage.resource), &staticGetResource_urlMatchesp, &staticGetResource_respond, &(rootResourceStorage.staticResource));
-	if(ready_fd) return 4;
+	if(status) return 3;
+	status = httpServer_pushResource(&server, &(rootResourceStorage.link_node), &(rootResourceStorage.resource), &staticGetResource_urlMatchesp, &staticGetResource_respond, &(rootResourceStorage.staticResource));
+	if(status) return 4;
 
-	ready_fd = -1;
 	if(httpServer_listen(&server, arguments_vector[1], 32)){
 		server.listeningSocket_fileDescriptor = -1;
 		httpServer_close(&server);

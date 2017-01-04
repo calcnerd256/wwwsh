aa3a7077ff87a7f1a4a6a6c42c9e9d455d61c433
Montana Rowe <calcnerd256@gmail.com>
2017-01-04 15:53:13 -0600

    moved a method and a visitor that really belongs to another class

--- src/main.c
+++ src/main.c
@@ -24,58 +24,6 @@ int init_connection(struct conn_bundle *ptr, struct httpServer *server, int fd){
 
 #include "./visit_connection_bundle_process_step.c"
 
-int visit_connection_bundle_select_read(struct conn_bundle *conn, struct linked_list *context, struct linked_list *node){
-	int *done;
-	int *fd;
-	struct timeval timeout;
-	fd_set to_read;
-	int status;
-	(void)node;
-	if(!context) return 1;
-	if(!context->next) return 1;
-	done = (int *)(context->data);
-	if(!done) return 3;
-	if(*done) return 0;
-	fd = (int *)(context->next->data);
-	if(!fd) return 3;
-	if(!conn) return 2;
-	if(conn->input.done) return 0;
-	if(-1 == conn->fd) return 2;
-	timeout.tv_sec = 0;
-	timeout.tv_usec = 0;
-	FD_ZERO(&to_read);
-	FD_SET(conn->fd, &to_read);
-	status = select(conn->fd + 1, &to_read, 0, 0, &timeout);
-	if(-1 == status) return 4;
-	if(!status) return 0;
-	if(FD_ISSET(conn->fd, &to_read)){
-		*fd = conn->fd;
-		*done = 1;
-	}
-	return 0;
-}
-
-int httpServer_selectRead(struct httpServer *server){
-	int ready_fd;
-	struct linked_list context;
-	struct linked_list fd_cell;
-	struct conn_bundle fake_for_server;
-	struct linked_list extra_head;
-	int done;
-	done = 0;
-	ready_fd = -1;
-	fd_cell.next = 0;
-	fd_cell.data = &ready_fd;
-	context.next = &fd_cell;
-	context.data = &done;
-	extra_head.next = server->connections;
-	extra_head.data = &fake_for_server;
-	fake_for_server.fd = server->listeningSocket_fileDescriptor;
-	fake_for_server.input.done = 0;
-	if(traverse_linked_list(&extra_head, (visitor_t)(&visit_connection_bundle_select_read), &context)) return -1;
-	return ready_fd;
-}
-
 int httpServer_removeEmptyConnections(struct httpServer *server){
 	struct linked_list *node = 0;
 	struct linked_list *middle = 0;

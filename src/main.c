654ff38952cae55e59078a3c9631e4ca2e5a9ba7
Montana Rowe <calcnerd256@gmail.com>
2017-01-04 13:56:20 -0600

    inlined a helper

--- src/main.c
+++ src/main.c
@@ -99,15 +99,6 @@ int httpServer_pushResource(struct httpServer *server, struct linked_list *new_h
 	return 0;
 }
 
-int staticGetResource_initHtml(struct staticGetResource *resource, char* url, char* body, struct extent *urlStorage, struct extent *bodyStorage, struct linked_list *headerHeadStorage, struct linked_list *otherHeaders, struct linked_list *keyNode, struct linked_list *valueNode, struct extent *key, struct extent *value){
-	point_extent_at_nice_string(urlStorage, url);
-	point_extent_at_nice_string(bodyStorage, body);
-	resource->url = urlStorage;
-	resource->body = bodyStorage;
-	resource->headers = push_header_nice_strings(headerHeadStorage, keyNode, valueNode, key, "Content-Type", value, "text/html", otherHeaders);
-	return !(resource->headers);
-}
-
 int httpServer_init(struct httpServer *server, struct mempool *pool){
 	server->listeningSocket_fileDescriptor = -1;
 	server->memoryPool = pool;
@@ -313,20 +304,13 @@ int main(int argument_count, char* *arguments_vector){
 	if(2 != argument_count) return 1;
 	if(httpServer_init(&server, &serverAllocations)) return 2;
 
-	ready_fd = staticGetResource_initHtml(
-		&rootResourceStorage_staticResource,
-		"/",
-		"<html>\r\n <head>\r\n  <title>Hello World!</title>\r\n </head>\r\n <body>\r\n  <h1>Hello, World!</h1>\r\n  <p>\r\n   This webserver is written in C.\r\n   I'm pretty proud of it!\r\n  </p>\r\n  <p>Coming soon: a resource for the following form to POST to</p>\r\n  <form method=\"POST\" action=\"formtest/\">\r\n   <input type=\"submit\" />\r\n  </form>\r\n </body>\r\n</html>\r\n\r\n",
-		&rootResourceStorage_url,
-		&rootResourceStorage_body,
-		&rootResourceStorage_headerHead,
-		0,
-		&rootResourceStorage_keyNode,
-		&rootResourceStorage_valNode,
-		&rootResourceStorage_key,
-		&rootResourceStorage_val
-	);
-	if(ready_fd) return 3;
+	if(point_extent_at_nice_string(&rootResourceStorage_url, "/")) return 3;
+	if(point_extent_at_nice_string(&rootResourceStorage_body, "<html>\r\n <head>\r\n  <title>Hello World!</title>\r\n </head>\r\n <body>\r\n  <h1>Hello, World!</h1>\r\n  <p>\r\n   This webserver is written in C.\r\n   I'm pretty proud of it!\r\n  </p>\r\n  <p>Coming soon: a resource for the following form to POST to</p>\r\n  <form method=\"POST\" action=\"formtest/\">\r\n   <input type=\"submit\" />\r\n  </form>\r\n </body>\r\n</html>\r\n\r\n"))
+		return 3;
+	rootResourceStorage_staticResource.url = &rootResourceStorage_url;
+	rootResourceStorage_staticResource.body = &rootResourceStorage_body;
+	rootResourceStorage_staticResource.headers = push_header_nice_strings(&rootResourceStorage_headerHead, &rootResourceStorage_keyNode, &rootResourceStorage_valNode, &rootResourceStorage_key, "Content-Type", &rootResourceStorage_val, "text/html", 0);
+	if(!(rootResourceStorage_staticResource.headers)) return 3;
 	ready_fd = httpServer_pushResource(&server, &rootResourceStorage_newHead, &rootResourceStorage_resource, &staticGetResource_urlMatchesp, &staticGetResource_respond, &rootResourceStorage_staticResource);
 	if(ready_fd) return 4;
 

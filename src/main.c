df18f8a5cb3acc9e259c54cce49a3c6b616da554
Montana Rowe <calcnerd256@gmail.com>
2017-01-04 18:02:21 -0600

    a little bit of cleanup

--- src/main.c
+++ src/main.c
@@ -21,10 +21,33 @@ int main(int argument_count, char* *arguments_vector){
 	status = contiguousHtmlResource_init(
 		&rootResourceStorage,
 		"/",
-		"<html>\r\n <head>\r\n  <title>Hello World!</title>\r\n </head>\r\n <body>\r\n  <h1>Hello, World!</h1>\r\n  <p>\r\n   This webserver is written in C.\r\n   I'm pretty proud of it!\r\n  </p>\r\n  <p>Coming soon: a resource for the following form to POST to</p>\r\n  <form method=\"POST\" action=\"formtest/\">\r\n   <input type=\"submit\" />\r\n  </form>\r\n </body>\r\n</html>\r\n\r\n"
+		"<html>\r\n"
+		" <head>\r\n"
+		"  <title>Hello World!</title>\r\n"
+		" </head>\r\n"
+		" <body>\r\n"
+		"  <h1>Hello, World!</h1>\r\n"
+		"  <p>\r\n"
+		"   This webserver is written in C.\r\n"
+		"   I'm pretty proud of it!\r\n"
+		"  </p>\r\n"
+		"  <p>Coming soon: a resource for the following form to POST to</p>\r\n"
+		"  <form method=\"POST\" action=\"formtest/\">\r\n"
+		"   <input type=\"submit\" />\r\n"
+		"  </form>\r\n"
+		" </body>\r\n"
+		"</html>\r\n"
+		"\r\n"
 	);
 	if(status) return 3;
-	status = httpServer_pushResource(&server, &(rootResourceStorage.link_node), &(rootResourceStorage.resource), &staticGetResource_urlMatchesp, &staticGetResource_respond, &(rootResourceStorage.staticResource));
+	status = httpServer_pushResource(
+		&server,
+		&(rootResourceStorage.link_node),
+		&(rootResourceStorage.resource),
+		&staticGetResource_urlMatchesp,
+		&staticGetResource_respond,
+		&(rootResourceStorage.staticResource)
+	);
 	if(status) return 4;
 
 	if(httpServer_listen(&server, arguments_vector[1], 32)){
@@ -40,7 +63,8 @@ int main(int argument_count, char* *arguments_vector){
 				httpServer_acceptNewConnection(&server);
 			else{
 				match_node = 0;
-				if(!first_matching(server.connections, (visitor_t)(&match_by_sockfd), (struct linked_list *)(&ready_fd), &match_node))
+				status = first_matching(server.connections, (visitor_t)(&match_by_sockfd), (struct linked_list *)(&ready_fd), &match_node);
+				if(!status)
 					if(match_node)
 						httpRequestHandler_readChunk(match_node->data);
 			}

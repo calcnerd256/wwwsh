d06b1cbf24cbe0a4286fd2b503c8e6a3bc10d00a
Montana Rowe <calcnerd256@gmail.com>
2017-02-03 16:36:54 -0600

    gained cdecl dependency because not all declarations concatenate

--- bin/struct.bash
+++ bin/struct.bash
@@ -13,7 +13,7 @@ echo
 echo "struct $STRUCT_IDENTIFIER{"
 for FIELD_NAME in $(cat ./fields); do
 	FIELD_TYPE="$(cat "$FIELD_NAME".field)"
-	FIELD_DECL="$(echo "$FIELD_TYPE $FIELD_NAME" | sed "s/ \* / */")"
+	FIELD_DECL="$(echo "explain ($FIELD_TYPE)_" | cdecl | sed "s/cast _ into/declare $FIELD_NAME as/" | cdecl)"
 	echo -e "\t$FIELD_DECL;"
 done
 echo "};"

952fbbc0e0b514977ce634d10730ce07a097900b
Montana Rowe <calcnerd256@gmail.com>
2017-02-06 07:01:07 -0600

    inlined indirect include

--- src/extent/linkedList_headers.h
+++ src/extent/linkedList_headers.h
@@ -1,3 +1,25 @@
 /* -*- indent-tabs-mode: t; tab-width: 2; c-basic-offset: 2; c-default-style: "stroustrup"; -*- */
 
-#include "../linkedList/headers.h"
+#include "../linkedList/linkedList.struct.h"
+#include "../linkedList/visitor_t.typedef.h"
+
+
+#include "../linkedList/alloc_copy_visitor.h"
+#include "../linkedList/free_visitor_copy.h"
+#include "../linkedList/apply_visitor.h"
+#include "../linkedList/traverse_linked_list.h"
+#include "../linkedList/clean_and_free_list.h"
+#include "../linkedList/visit_matcher.h"
+#include "../linkedList/first_matching.h"
+#include "../linkedList/match_last.h"
+#include "../linkedList/last_node.h"
+#include "../linkedList/append.h"
+
+#include "../linkedList/popEmptyFreeing.linkedList.h"
+#include "../linkedList/removeMiddleEmptiesFreeing.linkedList.h"
+
+
+#include "../linkedList/dequoid.struct.h"
+
+#include "../linkedList/init.dequoid.h"
+#include "../linkedList/append.dequoid.h"

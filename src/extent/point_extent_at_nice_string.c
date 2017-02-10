2ef43a30edabbbacb0da861faa8000c416e1849a
Montana Rowe <calcnerd256@gmail.com>
2017-02-10 06:24:45 -0600

    narrowed includes and indirected one

--- src/extent/point_extent_at_nice_string.c
+++ src/extent/point_extent_at_nice_string.c
@@ -1,5 +1,9 @@
 /* -*- indent-tabs-mode: t; tab-width: 2; c-basic-offset: 2; c-default-style: "stroustrup"; -*- */
 
+#include "./point_extent_at_nice_string.h"
+#include "./extent.struct.h"
+#include "./strlen.h"
+
 int point_extent_at_nice_string(struct extent *storage, char *bytes){
 	storage->bytes = bytes;
 	storage->len = strlen(bytes);

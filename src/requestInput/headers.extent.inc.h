c0839c11221b1405d9b6d3b7f0737cbd690188b9
Montana Rowe <calcnerd256@gmail.com>
2017-02-11 15:28:51 -0600

    inlined a singleton include indirection and re-indirected its contents

--- src/requestInput/headers.extent.inc.h
+++ src/requestInput/headers.extent.inc.h
@@ -1,3 +1,18 @@
 /* -*- indent-tabs-mode: t; tab-width: 2; c-basic-offset: 2; c-default-style: "stroustrup"; -*- */
 
-#include "../extent/headers.h"
+#include "./extent.struct.h"
+
+#include "./point_extent_at_nice_string.h"
+#include "./visit_extent_sumLength.h"
+
+
+#include "./mempool.struct.h"
+
+
+#include "./init_pool.h"
+#include "./palloc.h"
+#include "./free_pool.h"
+
+
+#include "./sys_types.inc.h"
+#include "./headers.linkedList.inc.h"
---
+++ src/requestInput/point_extent_at_nice_string.h
@@ -0,0 +1,3 @@
+/* -*- indent-tabs-mode: t; tab-width: 2; c-basic-offset: 2; c-default-style: "stroustrup"; -*- */
+
+#include "../extent/point_extent_at_nice_string.h"
